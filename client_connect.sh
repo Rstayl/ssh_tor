b39190247af85816e6a148eb9cedf9b237c3d016
Rstayl <b.o.s.adebisi@hotmail.fr>
2020-09-24 17:08:27 +0200

    add client configuration and connection

---
+++ client_connect.sh
@@ -0,0 +1,10 @@
+echo "[INFO] Install Tor, ssh, nmap and ncap"
+
+apt update && apt install -y tor nmap ncap
+
+read -p "Enter the user name of your requested host: "  user
+
+read -p "Enter the .onion of requested host: "  onion
+ssh -o ProxyCommand="ncat --proxy 127.0.0.1:9050 --proxy-type socks5 %h %p" $user@$onion
+
+echo "Welcome to your onion host"
