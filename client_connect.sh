6b93edd1eda403f502c98e46898ca395fb07fcf4
Rstayl <b.o.s.adebisi@hotmail.fr>
2020-10-09 11:21:08 +0200

    update

--- client_connect.sh
+++ client_connect.sh
@@ -5,6 +5,7 @@ apt update && apt install -y tor nmap ncap
 read -p "Enter the user name of your requested host: "  user
 
 read -p "Enter the .onion of requested host: "  onion
-ssh -o ProxyCommand="ncat --proxy 127.0.0.1:9050 --proxy-type socks5 %h %p" $user@$onion
+
+ssh -o ProxyCommand="ncap --proxy 127.0.0.1:9050 --proxy-type socks5 %h %p" $user@$onion
 
 echo "Welcome to your onion host"
